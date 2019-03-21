028b2ec060c136ca5b4c366d9644694cd9cbfaef
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-03-21 12:55:48 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -85,11 +85,11 @@ void read_points(char *argv[]) {
 void distrib_points() {
 
 	int* send_counts						// an array of how many points each process will recieve / how many root sends
-		 = allocate (sizeof(int) * nprocs);
+		 = (int*) allocate (sizeof(int) * nprocs);
 	int points_to_recv = 0;					// used to store a single number from send_counts
-	int displs_point_scatter				// the displacements for the scatterv, significant only to root
-		 = allocate (sizeof(int) * nprocs);
-		 
+	int* displs_point_scatter				// the displacements for the scatterv, significant only to root
+		 = (int*) allocate (sizeof(int) * nprocs);
+
 	if (my_rank==ROOT) {
 		// use interger division to determin the base amount for points each process will recieve 
 		long base_point_count = num_points/(long)nprocs;
