73feba77bb347daaca9bea3120e6ce9bc8d75eb9
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-03-21 16:10:02 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -208,8 +208,8 @@ void gen_lines() {
 			}
 			double* temp = (double*) allocate(sizeof(d_my_lines)+sizeof(d_new_lines));
 			int num_my_linr = sizeof(d_my_lines)/sizeof(line_t);
-			copy_array(d_my_lines, temp, num_my_linr);
-			copy_array(d_new_lines, &temp[num_my_linr], sizeof(d_new_lines)/sizeof(line_t));
+			copy_array(&d_my_lines, &temp, num_my_linr);
+			copy_array(&d_new_lines, &temp[num_my_linr], sizeof(d_new_lines)/sizeof(line_t));
 
 			free(d_my_lines);
 			d_my_lines = allocate(sizeof(temp));
