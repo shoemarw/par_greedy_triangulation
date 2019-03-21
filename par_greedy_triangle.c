08f353536904ad727b3098c07435898420af0649
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-03-21 13:27:51 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -85,12 +85,14 @@ void read_points(char *argv[]) {
 void distrib_points() {
 
 	int* send_counts						// an array of how many points each process will recieve / how many root sends
-		 = (int*) allocate (sizeof(int) * nprocs);
+		 //= (int*) allocate (sizeof(int) * nprocs);
 	int points_to_recv = 0;					// used to store a single number from send_counts
 	int* displs_point_scatter;				// the displacements for the scatterv, significant only to root
 		 //= (int*) allocate (sizeof(int) * nprocs);
 
 	if (my_rank==ROOT) {
+		displs_point_scatter = (int*) allocate (sizeof(int) * nprocs);
+		send_counts = = (int*) allocate (sizeof(int) * nprocs);
 		// use interger division to determine the base amount for points each process will recieve 
 		long base_point_count = num_points/(long)nprocs;
 
@@ -109,7 +111,6 @@ void distrib_points() {
 		} // end for
 
 		// build displacement array
-		displs_point_scatter = (int*) allocate (sizeof(int) * nprocs);
 		displs_point_scatter[0] = 0;
 		for (int i = 1; i < nprocs; i++) {
 			displs_point_scatter[i] = displs_point_scatter[i-1] + send_counts[i-1];
@@ -138,8 +139,8 @@ void distrib_points() {
 		MPI_Scatterv(points, send_counts, displs_point_scatter, MPI_point_t, my_points, points_to_recv,
                  MPI_point_t, ROOT, MPI_COMM_WORLD);
 	}
-	free(displs_point_scatter);
-	free(send_counts);
+	// free(displs_point_scatter);
+	// free(send_counts);
 }
 
 
