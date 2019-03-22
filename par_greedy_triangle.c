5dc5ece686f3d133d03735b9ef714cc3c1d14d7d
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-03-22 15:08:31 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -59,7 +59,7 @@ point_t* points;
 void double_array_to_struct(double* arr, line_t* new_arr, long size){
 	printf("%ld\n", size);
 	long index = 0;
-	for (long i = 0; i < size; i+=5) {
+	for (long i = 0; i < size*5; i+=5) {
 		point_t *p0 = (point_t*) allocate(sizeof(point_t));
 		point_t *p1 = (point_t*) allocate(sizeof(point_t));
 		p0->x = arr[i+X0];
