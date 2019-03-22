8aac422b51dacc6a0c158115c5f074e0fdded14c
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-03-22 09:37:14 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -33,6 +33,13 @@
 #define Y1  3	// second point's y 		| line_array[i + Y0] would be the i-th line's 
 #define LEN 4	// distance the points 		| y-coordinate for first point in the line.
 
+line_t* triang; // Will store the triangulation. Should only be used by proc 0.
+long tlines;    // Will keep track of how many lines are in the triangulation at
+                // any given time.
+bool finished;  // Will be true when the triangulation is complete.
+double[5] IMPOSSIBLE_LINE = [0, 0, 0, 0, -1]; // Processes send this during 
+
+
 int nprocs;         	// number of processes
 int my_rank;        	// rank of a particular process
 point_t* pt_my_points; 	// a processes' portion of the point set.
@@ -367,6 +374,135 @@ void distrib_lines() {
 }
 
 
+/*
+ * Builds the Greedy Triangulation. 
+ */
+void triangulate() {
+	// The triangulation will be stored as an array of lines. The triangulation
+	// is built on process zero iteratively as successive global minimal lines
+	// are found. Allocate enough space to potentially hold every line.
+	triang = (line_t*) allocate(num_line_count*sizeof(line_t));
+
+	// my_unknown is each local processes' number of lines whose status is 
+	// unknown. So it counts the number of lines that may or may not belong
+	// to the triangulation that a give process has. Initially all of a 
+	// processes' lines have unknown status.
+	long my_unknown = my_line_count;
+	tlines = 0; // Set the number of lines currently in the triangulation to 0.
+	// Keep participating in global communications until all processes have
+	// resolved the status of their local set of lines.
+	while (!finished) {
+		// If this process still has lines of unkown status it must
+		// work to resolve them.
+		if (my_unknown > 0) {
+			// Convert this processes' minimal (smallest) line to an array of
+			// five doubles for Allgather.
+			double my_min_line[5];
+			point_t p = *(ln_my_lines[0].p);
+			point_t q = *(ln_my_lines[0].q);
+			my_min_line[0] = p.x;
+			my_min_line[1] = p.y;
+			my_min_line[2] = q.x;
+			my_min_line[3] = q.y;
+			// Prepare an array to receive each processe's minimal line.
+			double* recv_buf = (double*) allocate(5*nprocs);
+			// Make sure each process has an array of each processes' min line.
+			MPI_Allgather(my_min_line, 5, MPI_DOUBLE, 
+				          recv_buf, 5, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+			// Find the global minimal line.
+			int min_line_index = 0; // Will hold index of the global min line.
+			for (int i = 0; i < nprocs, i++) {
+				// Compare the lenth of the current smallest line to the
+				// i^th line's length. If the length is not positive ignore it
+				// because it was a special value sent from a process with no
+				// more lines of unknown status.
+				if ((recv_buf[i*4] > 0) && 
+					(recv_buf[i*4] < recv_buf[min_line_index*4])) {
+					min_line_index = i;
+				}
+			}
+			// Will hold the minimal line.
+			line_t* min_line;
+			// Lets us know whether or not this processes min line was
+			// included in the triangulation or not.
+			int start;
+			// See if this process has the global min line, if so we must
+			// adjust its number of lines of unknown status and set min_line.
+			if (my_rank == min_line_index) {
+				my_unknown--;
+				min_line = ln_my_lines[0];
+				start = 1; // This processes' min was used.
+			// Otherwise we must build the min_line from data in the recv_buf
+			// (While making sure to use the appropriate index!)
+			} else {
+				// Get the minimal line
+				min_line = (line_t*) allocate(sizeof(line_t));
+				point_t *p = (point_t*) allocate(sizeof(point_t));
+				point_t *q = (point_t*) allocate(sizeof(point_t));
+				p->x = recv_buf[min_line_index*4];
+				p->y = recv_buf[min_line_index*4 + 1];
+				q->x = recv_buf[min_line_index*4 + 2];
+				q->y = recv_buf[min_line_index*4 + 3];
+				min_line->p = p;
+				min_line->q = q;
+				free(p);
+				free(q);
+				start = 0; // This processes' min was not used.
+			}
+			// Have process zero add min_line to the triangulation.
+			if (my_rank == 0) {
+				triang[tlines] = min_line;
+			}
+			// Free the receive buffer
+			free(recv_buf);
+			// Allocate an array of lines to hold the lines that don't 
+			// intersect with the global minimum.
+			line_t* temp = (line_t*) allocate(my_line_count*sizeof(line_t));
+			int end = my_unknown;
+			int temp_size = 0;
+
+			for (int j = start; j < end, j++) {
+				// Run the intersection test and only include lines which dont
+				// conflict with the global min (min_line) It is ok if a line
+				// shares endpoints with min_line
+				if (share_endpoint(min_line, ln_my_lines[j]) ||
+					 !intersects(min_line, ln_my_lines[j])) {
+					temp[temp_size] = ln_my_lines[j]
+					temp_size++;
+				} else {
+					my_unknown--;
+				}	
+			}
+			// Write all of the valid lines from temp to ln_my_lines to prepare
+			// for the next iteration.		
+			copy_array(temp, ln_my_lines, temp_size);
+			free(temp);
+		// If this process has no more lines of unknown status then it must still
+	    // participate in global communications to avoid deadlock. Have it send
+		// a special value to the other processes (which they will ignore). The 
+		// special value is a line whose endpoints are at the origin and it has a
+		// distance of -1.
+		} else {
+			// Prepare an array to receive each processe's minimal line.
+			double* recv_buf = (double*) allocate(5*nprocs);
+			MPI_Allgather(IMPOSSIBLE_LINE, 5, MPI_DOUBLE, 
+				          recv_buf, 5, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+			// Check if all of the lines have distance of -1. If so then we are
+			// done!
+			int count = 0;
+			for (int i = 0; i < nprocs; i++) {
+				if (recv_buf[i*4] == -1) {
+					count++;
+				}
+			}
+			if (count == nprocs) {
+				finished = true;
+			}
+			free(recv_buf); 
+		}
+	}
+}
+
 
 
 ////			  //
@@ -426,7 +562,8 @@ int main(int argc, char *argv[]) {
 	//                                      //
 	
 	START_TIMER(sort)
-	// qsort(ln_my_lines, (sizeof(ln_my_lines)/sizeof(line_t)), sizeof(line_t), compare);
+	qsort(ln_my_lines, (sizeof(ln_my_lines)/sizeof(line_t)), 
+		                sizeof(line_t), compare);
 	STOP_TIMER(sort)
 	
 	  //                                   //
@@ -434,13 +571,14 @@ int main(int argc, char *argv[]) {
     //	                                 //
 	
 	START_TIMER(triangulate)
-
+	triangulate();
 	STOP_TIMER(triangulate)
 	
-	  //                                     //
-     //  Triangulation Done, Display Stats  //
-    //	                                   //
-	
+	   //                                        //
+      //  Triangulation Done, Display Stats     //
+     //	  Create image & Output File in proc0  //
+	//                                        //
+
 	// These stats are only for the portions of the code specific to the three
 	// phases of building the greedy triangulation. Generate all lines, sort the
 	// lines in non-decreasing order, and greedily adding line segments to the
@@ -449,12 +587,35 @@ int main(int argc, char *argv[]) {
 		printf("Gent: %.4f  Sort: %.4f  Tria: %.4f Overhead: %.4f\n",
 	        GET_TIMER(generate), GET_TIMER(sort), 
 	        GET_TIMER(triangulate), GET_TIMER(MPIoverhead));
+
+# ifdef IMAGE	
+		generate_image(triang, tlines);
+# endif
+
+		// Store the triangulation in a file. Each line in the file corresponds to 
+		// a line in the triangulation. Each line consists of two tuples representing
+		// the end points of the line.
+		FILE* write_file = open_file("triangle_result.txt", "w");
+		
+		// The first line of the file specifies the number of lines in the file.
+		fprintf(write_file, "%ld\n", tlines);
+		
+		// Write triangulation to file for turtle processing
+		for (int i = 0; i < tlines; i++)
+		{
+		    point_t p = *(triang[i].p);
+		    point_t q = *(triang[i].q);
+		    fprintf(write_file, "(%lf, %lf) (%lf, %lf)\n", p.x, p.y, q.x, q.y);
+		}
+		
+		fclose(write_file);
 	}
 	
 	// Clean up and exit
 	if (my_rank == ROOT)
 		free(points);
-	// free(lines);
+
+	free(triang);
 	MPI_Finalize();
 	return (EXIT_SUCCESS);
 }
