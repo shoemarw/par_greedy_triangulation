3666788bf3c30ef68ab9f18d8a6902230b1c021a
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-03-22 02:10:29 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -344,9 +344,9 @@ void distrib_lines() {
 	d_my_lines = (double*) allocate(l_recv_doubs*sizeof(double));
 	printf("345 Hello from %d\n", my_rank);
 	// scatter lines
-	MPI_Scatterv(d_recv_lines, (int *)l_send_count, i_displs, MPI_DOUBLE, &d_my_lines, l_recv_doubs, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+	MPI_Scatterv(d_recv_lines, l_send_count, i_displs, MPI_DOUBLE, &d_my_lines, l_recv_doubs, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
 	printf("348 Hello from %d\n", my_rank);
-	
+
 	printf("my_line_count %ld\n", my_line_count);
 	// ln_my_lines = (line_t *) allocate(my_line_count*sizeof(line_t));
 
