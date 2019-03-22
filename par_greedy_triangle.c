2b49e155153d0a505dd873c1f924341dac368be5
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-03-21 23:49:12 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -157,10 +157,10 @@ void gen_lines() {
 	// 				  //
 
 	// Calculate how many lines that will be created	
-	int i_num_lines = ((my_point_count-1)*(my_point_count-2))/2;
+	my_line_count = ((my_point_count-1)*(my_point_count-2))/2;
 
 	// Allocate room for the lines that will be created
-	d_my_lines = (double*) allocate(i_num_lines * sizeof(double) * 5);
+	d_my_lines = (double*) allocate(my_line_count * sizeof(double) * 5);
 
 
 	long l_index = 0;
