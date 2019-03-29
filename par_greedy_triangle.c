45acab5b61ac4dbbb022c5a31af83220e3f89d9d
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-03-29 16:51:27 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -627,10 +627,10 @@ int main(int argc, char *argv[]) {
 	MPI_Barrier(MPI_COMM_WORLD);
 	START_TIMER(generate)
 	gen_lines();
-printf("Finished gen_lines()\n", );
+printf("Finished gen_lines()\n");
 
 	distrib_lines();
-printf("Finished distrib_lines()\n", );
+printf("Finished distrib_lines()\n");
 	MPI_Barrier(MPI_COMM_WORLD);
 	STOP_TIMER(generate)
 
@@ -640,7 +640,7 @@ printf("Finished distrib_lines()\n", );
 	MPI_Barrier(MPI_COMM_WORLD);
 	START_TIMER(sort)
 	qsort(ln_my_lines, my_line_count, sizeof(line_t), compare);
-printf("Finished qsort()\n", );
+printf("Finished qsort()\n");
 	MPI_Barrier(MPI_COMM_WORLD);
 	STOP_TIMER(sort)
 
