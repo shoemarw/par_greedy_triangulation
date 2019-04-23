db7c4da9d7f4c5c28753b472e9519db849eabe55
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-04-23 18:37:50 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -43,12 +43,10 @@ double IMPOSSIBLE_LINE[5] = {0, 0, 0, 0, -1}; // Processes send this during
 
 int nprocs;         	// number of processes
 int my_rank;        	// rank of a particular process
-point_t* pt_my_points; 	// a processes' portion of the point set.
 line_t*  ln_my_lines;  	// a processes' portion of lines.
 double* d_my_lines;		// a processes' portion of lines while in double format.
+double* d_all_lines;	// all lines in double format, used by root
 
-
-long my_point_count;	// Count of how many points a processes is responsible for
 long my_line_count = 0; // Count of how many lines a processes is responsible for
 
 //TEMPORARILY MAKING THESE GLOBAL, THIS MAY NEED TO BE CHANGED
@@ -92,7 +90,7 @@ void read_points(char *argv[]) {
 	
 	// Read in and store the point s.
 	double x, y;     // The Cartesian coordinates of a point.
-	long i = 0;    // Index for storing points.
+	long i = 0;      // Index for storing points.
 
 	while (fscanf(fin, "%lf %lf\n", &x, &y) == 2) {
 		// Put the values in a point struct and store.
@@ -114,225 +112,43 @@ void read_points(char *argv[]) {
 
 }
 
+gen_lines() {
+	  //                      //
+	 //  Generate all lines  //
+	//                      //
+	
+	// Make all possible line segments between the points
+	// and compute the length of each line.
+	// Compute the number of lines, note that the below formula
+	// will always resolve as an int because one of num_points or
+	// num_points-1 will be divisible by 2.
+	int num_lines = ((num_points)*(num_points-1))/2;
 
-void distrib_points() {
-	int i_send_count_bytes[nprocs];	// an array of how bytes each process will receive; significant only to root
-	int i_displs_p[nprocs];			// the displacements for the scatterv; significant only to root
+	d_all_lines = (double*) allocate(sizeof(double)*num_lines*5);	
 
-	if (my_rank==ROOT) {
-		// use integer division to determine the base amount for points each process will receive 
-		long base_point_count = l_num_points/(long)nprocs;
+	long index = 0;
+	for (int i = 0; i < num_points; i++) {
+		// Compute the distance between point i and every point
+		// from i+1 onward. Then 'make' and store the corresponding
+		// line.
+		for (int j = i+1; j < num_points; j++) {
 
-		// get the remainder to see how many leftover points there are
-		int remainder = l_num_points%nprocs;
+			double length = distance(&points[i], &points[j]);
 
-		// fill the array with the base number, then if there are remainders left add one to the 
-		// count of how many points the process will receive.
-		for (int i = 0; i < nprocs; i++) {
-			i_send_count_bytes[i] = base_point_count * sizeof(point_t);
-			if (remainder > 0) {
-				i_send_count_bytes[i] += 1 * sizeof(point_t);
-				remainder--;
-			} // end if
-		} // end for
+			d_all_lines[index*5+X0] = points[i].x;
+			d_all_lines[index*5+Y0] = points[i].y;
+			d_all_lines[index*5+X1] = points[j].x;
+			d_all_lines[index*5+Y1] = points[j].y;
+			d_all_lines[index*5+LEN] = length;
 
-		// build displacement array
-		i_displs_p[0] = 0;
-		for (int i = 1; i < nprocs; i++) {
-			i_displs_p[i] = i_displs_p[i-1] + i_send_count_bytes[i-1];
+			index++;
 		}
 	}
-	// send each process how many points it should expect
-	long bytes_to_expect;
-	MPI_Scatter(i_send_count_bytes, 1, MPI_INT, &bytes_to_expect, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
-
-	// calculate points a process is responsible for
-	my_point_count = bytes_to_expect/sizeof(point_t);
-
-	// Allocate room for the points about to be received
-	pt_my_points = (point_t*) allocate((int)(bytes_to_expect));
-
-	// send each process its points
-	MPI_Scatterv(points, i_send_count_bytes, i_displs_p, MPI_BYTE, pt_my_points, bytes_to_expect,
-             MPI_BYTE, ROOT, MPI_COMM_WORLD);
 }
 
 
 
-void gen_lines() {
-	  //				//
-	 // Generate lines //
-	// 				  //
-
-	// Calculate how many lines that will be created
-	my_line_count = ((my_point_count)*(my_point_count-1))/2;
-	// Allocate room for the lines that will be created
-	d_my_lines = (double*) allocate(my_line_count * sizeof(double) * 5);
-
-
-	long l_index = 0;
-
-	// Create lines in double format, i.e. a line is 5 doubles.
-	for (int i = 0; i < my_point_count; i++) {
-		// Compute the distance between point i and every point
-		// from i+1 onward. Then 'make' and store the corresponding
-		// line.
-		for (int j = i+1; j < my_point_count; j++) {
-			double length = distance(&pt_my_points[i], &pt_my_points[j]);
-			d_my_lines[l_index + X0] = pt_my_points[i].x;
-			d_my_lines[l_index + Y0] = pt_my_points[i].y;
-			d_my_lines[l_index + X1] = pt_my_points[j].x;
-			d_my_lines[l_index + Y1] = pt_my_points[j].y;
-			d_my_lines[l_index + LEN] = length;	
-
-			// Increment index to next line
-			l_index +=5;
-		}
-	}
-
-	  //													  //
-	 // Start sending/receiving points to create more lines  //
-	//														//
-	int point_recv_count; 	// Used to check how many objects will be sent in MPI_send
-	double* d_new_lines;
-	point_t* pt_new_points;
-	
-	// In this for loop we calculated all the remaining lines - which means we'll
-	// need to send/receive points from other processes. This is implemented
-	// in a binomial tree structure.
-	for (int iteration_square = 1; iteration_square < nprocs; iteration_square *= 2) {
-		// If process is a sender this iteration:
-		if (my_rank&iteration_square) {
-
-			// calculate the process number of whom to send to
-			int i_send_to = (my_rank-iteration_square+nprocs)%nprocs;
-
-			// send the number of points the receiver should expect
-			MPI_Send(&my_point_count, 1, MPI_INT, i_send_to, TAG, MPI_COMM_WORLD);
-
-			// send the points
-			MPI_Send(pt_my_points, my_point_count*sizeof(point_t), MPI_BYTE, i_send_to, TAG, MPI_COMM_WORLD);
-
-			free(pt_my_points);
-			break;  // done, nothing left for this process to do in this function
-		}
-		// If process is a receiver this iteration:
-		else {
-
-			// calculate the process number of whom to receive from
-			int i_recv_from = my_rank+iteration_square; // The process number to receive from
-			
-			// receive the number of points about to get sent
-			MPI_Recv(&point_recv_count, 1, MPI_INT, i_recv_from, MPI_ANY_TAG, MPI_COMM_WORLD, 
-					 MPI_STATUS_IGNORE);
-
-			// calculate how many points that will be received 
-			long bytes_to_recv = point_recv_count*sizeof(point_t);
-
-			// create a array for bytes/points about to be received
-			pt_new_points = (point_t*) allocate(bytes_to_recv);
-
-			// receive points into pt_new_points
-			MPI_Recv(pt_new_points, bytes_to_recv, MPI_BYTE, i_recv_from, MPI_ANY_TAG, 
-					 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-			// calculate number of new lines to be created
-			long new_line_count = my_point_count*point_recv_count;
-
-			// create array to store lines about to be created
-			d_new_lines = (double*) allocate(sizeof(double)*new_line_count*5);
-
-			// create long to keep tract of d_new_lines index through for loop
-			long new_line_index = 0;
-
-			// create new lines by taking one of the process's current lines and making lines from that point
-			// to all of the newly received points. This is done in a double for loop.
-			for (int j = 0; j < my_point_count; j++){
-				for (int k = 0; k < point_recv_count; ++k)
-				{
-	
-					double length = distance(&pt_my_points[j], &pt_new_points[k]);
-					d_new_lines[new_line_index + X0] = pt_my_points[j].x;
-					d_new_lines[new_line_index + Y0] = pt_my_points[j].y;
-					d_new_lines[new_line_index + X1] = pt_new_points[k].x;
-					d_new_lines[new_line_index + Y1] = pt_new_points[k].y;
-					d_new_lines[new_line_index + LEN] = length;
-
-					new_line_index +=5;
-				}
-			} // end out for
-
-
-			// merge d_my_lines with d_new_lines
-			double* temp_doubles = (double*) allocate((my_line_count+new_line_count)*sizeof(double)*5);
-			for (int i = 0; i < (my_line_count*5); i++) {
-				temp_doubles[i] = d_my_lines[i];
-			}
-			for(int i = 0; i < (new_line_count*5); i++) {
-				temp_doubles[i + my_line_count*5] = d_new_lines[i];
-			}
-			free(d_my_lines);
-			free(d_new_lines);
-			d_my_lines = temp_doubles;
-
-			// merge pt_my_points with pt_new_points
-			point_t* temp_points = (point_t*) allocate((my_point_count+point_recv_count)*sizeof(point_t));
-			for (int i = 0; i < my_point_count; i++) {
-				temp_points[i] = pt_my_points[i];
-			}
-			for(int i = 0; i < point_recv_count; i++) {
-				temp_points[i + my_point_count] = pt_new_points[i];
-			}
-			free(pt_my_points);
-			free(pt_new_points);
-			pt_my_points = temp_points;
-
-			// update my_line_count	
-			my_line_count += new_line_count;
-
-			// update my point count
-			my_point_count += point_recv_count;
-
-		} // end of receiver branch of if
-	}// end for
-}// end of gen_lines
-
-
 void distrib_lines() {
-	int displs[nprocs];		// Used by root only
-	double* d_recv_lines;	// Used by root only
-	int* i_recv_counts;		// Used by root only
-
-	if (my_rank == ROOT) {
-		i_recv_counts = allocate (sizeof(int) * nprocs);
-	}
-
-	// send the number of lines a process will be sending on the gatherv
-	MPI_Gather(&my_line_count, 1, MPI_INT, i_recv_counts, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
-	long total_line_num;
-	if (my_rank == ROOT) {
-		for(int i = 0; i < nprocs; i++) {
-			i_recv_counts[i] *= 5;
-		}
-
-		displs[0] = 0;
-		total_line_num = i_recv_counts[0];
-
-		// calculate how many total lines are being sent and the displs
-        for (int i=1; i < nprocs; i++) {
-           	total_line_num += i_recv_counts[i];
-           	displs[i] = displs[i-1] + i_recv_counts[i-1];
-        }
-		d_recv_lines = (double*) allocate(total_line_num* sizeof(double));
-
-
-	}
-
-	MPI_Gatherv(d_my_lines, (my_line_count*5), MPI_DOUBLE, d_recv_lines, i_recv_counts, 
-			    displs, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
-
-
-
 	long l_base;
 	int remainder;
 	int *i_send_counts;
@@ -340,7 +156,6 @@ void distrib_lines() {
 	int *i_displs;
 
 	if(my_rank==ROOT) {
-		free(i_recv_counts);
 		my_line_count = total_line_num/5;
 
 	 	l_base = my_line_count/nprocs;		// Base number of lines to send (lines being 5 doubles)
