4ba6936ab4f1184fee3840fcacdb93877e8185ae
Eliza Shoemaker <eliza@Elizas-MacBook-Air.local>
2019-04-24 19:27:55 -0400

    Update par_greedy_triangle.c

--- par_greedy_triangle.c
+++ par_greedy_triangle.c
@@ -73,7 +73,7 @@ void double_array_to_struct(double* arr, line_t* new_arr, long size){
 
 		// Put first point into struct
 		l->p = &points_to_free[points_index];
-		free(p0);
+		// free(p0);
 		points_index++;
 
 
@@ -87,7 +87,7 @@ void double_array_to_struct(double* arr, line_t* new_arr, long size){
 
 		// Put second point into struct
 		l->q = &points_to_free[points_index];
-		free(p1);
+		// free(p1);
 		points_index++;	
 
 
